125b1e0cb5c1e51ec7c84f79944bf8c35873b180
Hiforumi Tamori <Hirofumi.Tamori@outlook.com>
2017-12-26 01:25:11 +0900

    Add Sources

---
+++ src/main/index.dev.js
@@ -0,0 +1,41 @@
+/**
+ * This file is used specifically and only for development. It installs
+ * `electron-debug` & `vue-devtools`. There shouldn't be any need to
+ *  modify this file, but it can be used to extend your development
+ *  environment.
+ */
+
+/* eslint-disable */
+
+import cors_proxy from 'cors-anywhere'
+
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PORT || 8080;
+
+cors_proxy.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+// Set environment for development
+process.env.NODE_ENV = 'development'
+
+// Install `electron-debug` with `devtron`
+require('electron-debug')({ showDevTools: true })
+
+// Install `vue-devtools`
+require('electron').app.on('ready', () => {
+  let installExtension = require('electron-devtools-installer')
+  installExtension.default(installExtension.VUEJS_DEVTOOLS)
+    .then(() => {})
+    .catch(err => {
+      console.log('Unable to install `vue-devtools`: \n', err)
+    })
+})
+
+// Require `main` process to boot app
+require('./index')
---
+++ src/renderer/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <!-- <router-view></router-view> -->
+    <div class="block">
+      <h1> Hello ! </h1>
+      <span class="slider">
+        <el-slider v-model="valueSlider"></el-slider>
+      </span>
+    </div>
+    <div class="block">
+      <span class="slider">
+        <el-slider v-model="valueSlider2"></el-slider>
+      </span>
+    </div>
+    <div class="block">
+      <el-button plain @click="openURL">HTTP Access</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'webapi-sample1',
+    methods: {
+      openURL () {
+        this.$http.get('http://localhost:8080/http://www.yahoo.co.jp')
+          .then(response => {
+            console.log(response.data)
+          })
+      }
+    },
+    data () {
+      return {
+        valueSlider: 50,
+        valueSlider2: 50
+      }
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+  .block {
+    font-family:  "メイリオ",sans-serif;
+    color: #445566;
+  }
+  .slider {
+    width: 50%;
+    color: #998800;
+  }
+</style>
